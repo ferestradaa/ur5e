cd8317b1dfe975b753defcc5413da35f725c8cfe
Fernando Estrada <101828467+ferestradaa@users.noreply.github.com>
2025-06-11 15:36:45 +0900

    Add files via upload
    this version of scneario py uses a fbx file as osctable instead of fixed cuboid

--- ik_extension1/RmpFlow_Example_python/scenario.py
+++ ik_extension1/RmpFlow_Example_python/scenario.py
@@ -1,14 +1,8 @@
-# Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto. Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-#
 
 import numpy as np
 import os
+import carb 
+
 
 from omni.isaac.core.utils.extensions import get_extension_path_from_name
 from omni.isaac.core.utils.stage import add_reference_to_stage
@@ -24,6 +18,10 @@ from omni.isaac.motion_generation.interface_config_loader import (
     get_supported_robot_policy_pairs,
     load_supported_motion_policy_config,
 )
+from omni.isaac.core.prims import RigidPrim  
+
+if not hasattr(carb, "log_warning"):
+    carb.log_warning = carb.log_warn
 
 class FrankaRmpFlowExample():
     def __init__(self):
@@ -36,29 +34,34 @@ class FrankaRmpFlowExample():
         self._dbg_mode = False
 
     def load_example_assets(self):
-        # Add the Franka and target to the stage
+        # Add the Franka and target to the stage        
         # The position in which things are loaded is also the position in which they 
 
-        robot_prim_path = "/ur5e"
-        path_to_robot_usd = "test1_clean.usd"
-        add_reference_to_stage(path_to_robot_usd, robot_prim_path)
+        stage_path = '/World'
+        robot_prim_path = "/World/robot/test1_clean"
+        path_to_robot_usd = "/home/aist/Desktop/FES/scenario1/RmpFlow_Example_python/ur5e_world3.usd"
+        add_reference_to_stage(path_to_robot_usd, stage_path)
         self._articulation = Articulation(robot_prim_path)
-        
+
         #add_reference_to_stage(get_assets_root_path() + "/Isaac/Props/UIElements/frame_prim.usd", "/World/target")
         #self._target = XFormPrim("/World/target", scale=[.04,.04,.04],)
-        self._target = VisualCylinder("/World/target",scale=[0.04, 0.04, 0.06],color=np.array([0.1,0.0,0.0]))
+        
+        self._target = VisualCylinder("/World/target/target",scale=[0.04, 0.04, 0.06],color=np.array([0.1,0.0,0.0]))
+
+        self._obstacle = RigidPrim("/World/obstacles/table/AnyConv_com__table/instance_def_1_a8d2744e_c4ac_4c01_9007_d297f2c4fa8a", name="ObstacleTable")
+        #self._obstacle = FixedCuboid("/World/obstacles/",name="table_box",scale=[1.0, 1.5, 0.2],position=np.array([0.0, 0.0, -0.1]),color=np.array([0.,0.,0.1]))
         
         #self._parent = FixedCuboid("/World/obstacleFolder",scale=[0.0, 0.0, 0.0], position=np.array([0.0, 0.55, -0.1]))
 
-        self._obstacle = FixedCuboid("/World/obstacleFolder/obstacle",name="Obstacle",scale=[1.0, 1.36, 0.2],position=np.array([0.0, 0.55, -0.1]),color=np.array([0.,0.,0.1]))
-        self._obstacleTwo = FixedCuboid("/World/obstacleFolder/obstacleTwo",name="ObstacleTwo",scale=[0.23, 0.31, 0.22],position=np.array([-0.114, 0.5285, 0.1158]),color=np.array([0.,0.,0.1]))
-        self._obstacleThree = FixedCuboid("/World/obstacleFolder/BoxStacle",name="BoxStacle",scale=[0.33, 0.46, 0.1],position=np.array([0.2, 0.55302, 0.05358]),color=np.array([0.2,0.05,0.025]))
-        self._obstacleFour = FixedCuboid("/World/obstacleFolder/obstacleFour",name="ObstacleFour",scale=[1.2, 0.05, 1.85],position=np.array([0.0, -0.2565, 0.17187]),color=np.array([0.1,0.1,0.1]))
-        self._obstacleFive = FixedCuboid("/World/obstacleFolder/obstacleFive",name="ObstacleFive",scale=[0.05, 0.9, 1.85],position=np.array([-0.57, 0.27527, 0.172]),color=np.array([0.1, 0.1 ,0.1]))
+        #self._obstacleTwo = FixedCuboid("/World/obstacleFolder/obst    acleTwo",name="ObstacleTwo",scale=[0.23, 0.31, 0.22],position=np.array([-0.114, 0.5285, 0.1158]),color=np.array([0.,0.,0.1]))
+        #self._obstacleThree = FixedCuboid("/World/obstacleFolder/BoxStacle",name="BoxStacle",scale=[0.33, 0.46, 0.1],position=np.array([0.2, 0.55302, 0.05358]),color=np.array([0.2,0.05,0.025]))
+        #self._obstacleFour = FixedCuboid("/World/obstacleFolder/obstacleFour",name="ObstacleFour",scale=[1.2, 0.05, 1.85],position=np.array([0.0, -0.2565, 0.17187]),color=np.array([0.1,0.1,0.1]))
+        #self._obstacleFive = FixedCuboid("/World/obstacleFolder/obstacleFive",name="ObstacleFive",scale=[0.05, 0.9, 1.85],position=np.array([-0.57, 0.27527, 0.172]),color=np.array([0.1, 0.1 ,0.1]))
 
         # Return assets that were added to the stage so that they can be registered with the core.World
-        return self._articulation, self._target, self._obstacle, self._obstacleTwo, self._obstacleThree, self._obstacleFour, self._obstacleFive
-
+        #return self._articulation, self._target, self._obstacle, self._obstacleTwo, self._obstacleThree, self._obstacleFour, self._obstacleFive
+        return self._articulation, self._target, self._obstacle
+    
     def setup(self):
         # Loading RMPflow can be done quickly for supported robots
         print("Supported Robots with a Provided RMPflow Config:", list(get_supported_robot_policy_pairs().keys()))
@@ -66,11 +69,11 @@ class FrankaRmpFlowExample():
 
         #Initialize an RmpFlow object
         self._rmpflow = RmpFlow(**rmp_config)
-        self._rmpflow.add_obstacle(self._obstacle)
-        self._rmpflow.add_obstacle(self._obstacleTwo)
-        self._rmpflow.add_obstacle(self._obstacleThree)
-        self._rmpflow.add_obstacle(self._obstacleFour)
-        self._rmpflow.add_obstacle(self._obstacleFive)
+        self._rmpflow.add_obstacle(self._obstacle)  
+        #self._rmpflow.add_obstacle(self._obstacleTwo)
+        #self._rmpflow.add_obstacle(self._obstacleThree)
+        #self._rmpflow.add_obstacle(self._obstacleFour)
+        #self._rmpflow.add_obstacle(self._obstacleFive)
 
         if self._dbg_mode:
             self._rmpflow.set_ignore_state_updates(True)
